9afbd300021097cfd7917958451eeb0a7144d008
aymentigui <aymentigui@gmail.com>
2024-09-16 17:02:58 +0200

    add home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import Link from "next/link"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className="flex flex-col items-center justify-center h-screen w-full gap-8">
+      <Link className="font-semibold text-lg px-4 py-2 rounded-md text-gray-500 bg-lamaSky border" href="/admin"> Admin </Link>
+      <Link className="font-semibold text-lg px-4 py-2 rounded-md text-gray-500 bg-lamaPurple border" href="/teacher"> Teacher </Link>
+      <Link className="font-semibold text-lg px-4 py-2 rounded-md text-gray-500 bg-lamaYellow border" href="/parent"> Parent </Link>
+      <Link className="font-semibold text-lg px-4 py-2 rounded-md text-gray-500 bg-green-300 border" href="/student"> Student </Link>
+    </div>
   )
 }
 
